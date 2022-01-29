72c79be1f5d41a046ecfe0578ecb62043a3415ac
LightDream <radu.voinea.mihai@gmail.com>
2022-01-29 17:56:50 +0200

    +

---
+++ src/dev/lightdream/gtxcs/Frog.java
@@ -0,0 +1,4 @@
+package dev.lightdream.gtxcs;
+
+public class Frog {
+}
---
+++ src/dev/lightdream/gtxcs/Pond.java
@@ -0,0 +1,8 @@
+package dev.lightdream.gtxcs;
+
+public class Pond {
+    public static void main(String[] args) {
+
+
+    }
+}
